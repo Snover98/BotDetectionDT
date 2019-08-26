9f0fcc3b3f5c3024d516e7a5abc77bd60a36e657
benbanuz <benbanuz@users.noreply.github.com>
2019-08-26 16:30:35 +0300

    fix

--- create_db.py
+++ create_db.py
@@ -148,7 +148,7 @@ def get_tweets():
 
     for tweet in tweets:
         tweet_id = tweet[0]
-        tweet_text = tweet[1]
+        tweet_text = tweet[5]
         mentions_spesi = [mention[1] for mention in mentions if mention[0] == tweet_id]
         urls_spesi = [url[1] for url in urls if url[0] == tweet_id]
         df = df.append({"seq_idx": tweet_id, "seq": tweet_text, "mentions": mentions_spesi, "urls": urls_spesi},
