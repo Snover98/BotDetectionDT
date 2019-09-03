c6ef1f07b4ab2c1a5b829e3978f65380db39638a
benbanuz <benbanuz@users.noreply.github.com>
2019-09-03 19:44:00 +0300

    fix

--- dataset.py
+++ dataset.py
@@ -1,9 +1,10 @@
-import torch.utils.data.dataset as Dataset
+from torch.utils.data import Dataset
 import pandas as pd
 import torch
 import conn as c
 from user import User
 from create_db_to_rnn import is_eng
+import pickle
 
 
 def get_users(df: pd.DataFrame):
@@ -32,13 +33,25 @@ def get_users(df: pd.DataFrame):
 
 class UsersDataset(Dataset):
 
-    def __init__(self, csv_file):
+    def __init__(self, csv_file=None):
         """
         Args:
             csv_file (string): Path to the csv file with annotations.
         """
-        self.users_frame = pd.read_csv(csv_file)
-        self.users, self.labels = get_users(self.users_frame)
+        if csv_file is not None:
+            self.users_frame = pd.read_csv(csv_file)
+            self.users, self.labels = get_users(self.users_frame)
+
+            to_save = (self.users, self.labels)
+            file = open("users.pickle", 'wb')
+            pickle.dump(to_save, file)
+            file.close()
+
+        else:
+            file = open("users.pickle", 'rb')
+            to_save = pickle.load(file)
+            self.users, self.labels = to_save
+            file.close()
 
     def __len__(self):
         return len(self.users_frame)
@@ -48,3 +61,8 @@ class UsersDataset(Dataset):
             idx = idx.tolist()
 
         return self.users[idx], self.labels[idx]
+
+
+if __name__ == "__main__":
+    us_db = UsersDataset()
+
