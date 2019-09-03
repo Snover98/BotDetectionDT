054e1fbe55dc37206a24f7b4925f2ba69e15d14c
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-09-03 12:40:18 +0300

    made a test training code

--- main.py
+++ main.py
@@ -1,25 +1,32 @@
-import conn
-from user import User as US
-from tweet_feature_extractor import *
-from word_training import train_wtv_on_tweets
+import torch
+from torch.utils import data
+from torch import nn
+from dataset import UsersDataset
 from gensim.models import Word2Vec
 from classification_model import BotClassifier
+from torch.optim.adam import Adam
+from training import TorchTrainer, plot_fit
 
-api = conn.connect()
-user1 = US(api, 3098421349)
-user2 = US(api, 12052952)
-print("hi")
+num_epochs = 10
+batch_size = 8
+optim = Adam(lr=1e-3)
+loss_fn = nn.CrossEntropyLoss()
 
-# model = train_wtv_on_tweets()
-# model.save("word2vec.model")
-model = Word2Vec.load("word2vec.model")
-print('trained!')
-# ext = TweetFeatureExtractor(model, 100, 1024, 128)
-# print(ext([user1.tweets, user2.tweets]).shape)
-clf = BotClassifier(model, 100, 1024, 128, 1024)
-out = clf([user1, user2])
-print(out)
-print(out.shape)
+ds = UsersDataset(None)
+train_ratio = 0.8
+train_amount = int(train_ratio * len(ds))
+test_amount = len(ds) - train_amount
 
+train_set, test_set = data.random_split(ds, (train_amount, test_amount))
+train_dl = data.DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=2)
+test_dl = data.DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=2)
+
+w2v_model = Word2Vec.load("word2vec.model")
+clf = BotClassifier(w2v_model, 100, 1024, 128, 1024)
+
+trainer = TorchTrainer(clf, loss_fn, optim, device='cuda' if torch.cuda.is_available() else 'cpu')
+
+fit_res = trainer.fit(train_dl, test_dl, num_epochs, 'clf_checkpoint.model', 3)
+plot_fit(fit_res, legend='First Training')
 
 
