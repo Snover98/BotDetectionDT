d42634ade1a05c2eea0a62d179e4cff1aca5ddc0
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-11-02 16:46:44 +0300

    removed unsqueeze

--- data/utils.py
+++ data/utils.py
@@ -21,7 +21,7 @@ def get_tweets_avg_diffs(inputs):
             diffs.append(torch.Tensor(
                 [(tweet2.date - tweet1.date).seconds for tweet1, tweet2 in zip(user, user[1:])]).mean().unsqueeze(0))
         else:
-            diffs.append(torch.Tensor([0.0]).unsqueeze(0))
+            diffs.append(torch.Tensor([0.0]))
     return torch.cat(diffs)
 
 
