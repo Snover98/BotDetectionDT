73149cc1a64747c036be246e7d8c331639d8d140
Assaf-Haik-Barouch <Assaf98@gmail.com>
2019-11-02 13:43:48 +0300

    fixed relative paths in the project

--- training/train_w2v.py
+++ training/train_w2v.py
@@ -4,4 +4,4 @@ from data.dataset import UsersDataset
 if __name__ == "__main__":
     ds = UsersDataset(None)
     model = train_wtv_on_tweets(ds.users, 20)
-    model.save("../checkpoints/word2vec.model")
+    model.save("checkpoints/word2vec.model")
